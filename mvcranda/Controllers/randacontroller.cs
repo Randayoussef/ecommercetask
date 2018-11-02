d2f87fa05924365b9b989b12b76e8d6321d45d05
Randayoussef <44713065+Randayoussef@users.noreply.github.com>
2018-11-03 00:09:14 +0200

    initial

---
+++ mvcranda/Controllers/randacontroller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Mvcranda.controllers
+{
+    public class randaController : Controller
+    {
+        
+        
+
+        public string Index()
+        {
+            return "hello my name is randa ";
+        }
+
+        
+        
+
+        public string Welcome()
+        {
+            return "this is my welcome page ";
+        }
+    }
+}
